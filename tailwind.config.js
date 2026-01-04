ffbf51824f68d54056f5302a6070fd5ab38a28ac
Kenn <keanejuan07@gmail.com>
2026-01-04 15:43:24 +0700

    add livewire flux

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,8 @@ import forms from '@tailwindcss/forms';
 export default {
     content: [
         './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './vendor/livewire/flux/stubs/**/*.blade.php',
+        './vendor/livewire/flux-pro/stubs/**/*.blade.php',
         './storage/framework/views/*.php',
         './resources/views/**/*.blade.php',
     ],
